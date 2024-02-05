00cc2ac32ee8b18c6a55ed0a1cb25bc1b6eafe58
chaaerim <cofla411@ewhain.net>
2024-02-05 20:18:23 +0900

    practice prototype

--- closure.js
+++ closure.js
@@ -12,6 +12,8 @@ function range(start, end) {
 
   function getRange(start, end) {
     // 상위에 선언한 array를 사용하면 클로저를 통해 참조가 계속 가능하기 때문에 값이 쌓이게 됨.
+
+    // 함수 밖으로 분리 하는게 좋을 듯.. 함수 명에 맞게 동작하기 위해선
     let closureArr = [];
     if (end < start) {
       console.log(closureArr);
---
+++ prototype.js
@@ -0,0 +1,42 @@
+function randMax(max) {
+  return Math.trunc(1e9 * Math.random()) % max;
+}
+
+var reel = {
+  symbols: ['♠︎', '♥︎', '♦︎', '♣︎', '⛄︎', '⭐︎', '☾', '☼'],
+  spin() {
+    if (this.position == null) {
+      this.position = randMax(this.symbols.length - 1);
+    }
+    this.position = (this.position + 100 + randMax(100)) % this.symbols.length;
+  },
+  display() {
+    if (this.position == null) {
+      this.position = randMax(this.symbols.length - 1);
+    }
+    return this.symbols[this.position];
+  },
+};
+
+var slotMachine = {
+  reels: [Object.create(reel), Object.create(reel), Object.create(reel)],
+  spin() {
+    this.reels.forEach(function spinReel(reel) {
+      reel.spin();
+      console.log(reel);
+    });
+  },
+  display() {
+    this.reels.forEach(function displayReel(reel) {
+      const rightReel = Object.create(reel);
+      const leftReel = Object.create(reel);
+      rightReel.position = reel.position + 1;
+      leftReel.position = reel.position - 1;
+
+      console.log(leftReel.display(), reel.display(), rightReel.display());
+    });
+  },
+};
+
+slotMachine.spin();
+slotMachine.display();
